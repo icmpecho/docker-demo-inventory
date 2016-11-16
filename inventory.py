997e0ccad9f328eff91fa348da72574ad7ef93f6
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2016-11-17 00:42:49 +0700

    Print result to stdout

--- inventory.py
+++ inventory.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 @app.route('/inventory/<item_id>', methods=['GET'])
 def get_inventory(item_id):
     qty = random.randint(0, 1000)
+    print("GET <id: {}> Returns qty={}".format(item_id, qty))
     return jsonify(itemId=item_id, qty=qty)
